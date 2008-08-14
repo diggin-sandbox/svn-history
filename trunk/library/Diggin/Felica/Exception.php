bada99c93d802080c2214c5f5fa53acdcb7115d3
sasezaki <sasezaki@b45cdfdc-3f41-0410-be57-3dd150e55053>
2008-08-14 14:12:59 +0000

    Interactive→Wizard

--- trunk/library/Diggin/Felica/Exception.php
+++ trunk/library/Diggin/Felica/Exception.php
@@ -1,7 +1,22 @@
 <?php
 /**
+ * Diggin - Simplicity PHP Library
+ * 
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license.
+ * http://framework.zend.com/license/new-bsd
+ * 
  * @category   Diggin
  * @package    Diggin_Felica
+ * @copyright  2006-2008 sasezaki (http://diggin.musicrider.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class Diggin_Felica_Exception extends Exception 
+
+/**
+ * Diggin_Exectpion
+ */
+require_once 'Diggin/Exception.php';
+
+class Diggin_Felica_Exception extends Diggin_Exception 
 {}
