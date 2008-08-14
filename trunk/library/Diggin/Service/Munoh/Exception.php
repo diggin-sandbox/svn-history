dc0ea95fe7202fbbe9e8ea363236b68b6a7d30a2
sasezaki <sasezaki@b45cdfdc-3f41-0410-be57-3dd150e55053>
2008-08-14 15:07:03 +0000

    Service Exceptionまわり修正

--- trunk/library/Diggin/Service/Munoh/Exception.php
+++ trunk/library/Diggin/Service/Munoh/Exception.php
@@ -1,13 +1,24 @@
 <?php
 /**
- * @see Zend_Service_Exception
- */
-
-
-/**
+ * Diggin - Simplicity PHP Library
+ * 
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license.
+ * It is also available through the world-wide-web at this URL:
+ * http://framework.zend.com/license/new-bsd
+ * 
  * @category   Diggin
  * @package    Diggin_Service
- * @subpackage Munoh
+ * @subpackage Tumblr
+ * @copyright  2006-2008 sasezaki (http://diggin.musicrider.com)
+ * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
-class Diggin_Service_Munoh_Exception extends Exception
+
+/**
+ * @see Diggin_Service_Exception
+ */
+require_once 'Diggin/Service/Exception.php';
+
+class Diggin_Service_Munoh_Exception extends Diggin_Service_Exception
 {}
