9ec9ce67043cd6a1dec2dc0df7160dded031114f
sasezaki <sasezaki@b45cdfdc-3f41-0410-be57-3dd150e55053>
2008-09-27 07:32:23 +0000

    Flexible用デモ

---
+++ trunk/demos/Diggin/Scraper/uranai1.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'Zend/Loader.php';
+Zend_Loader::registerAutoload();
+
+$url = 'http://www.fujitv.co.jp/meza/uranai/';
+
+try {
+	    $ranking = new Diggin_Scraper_Process();
+		$ranking->addProcess('.', 'rank => [@background, "Digits"]')
+				->addProcess('img', 'star => @alt', 'image => @src')
+			    ->addProcess('td.text', 'text => TEXT')
+                ->addProcess('.lucky', 'lucky => TEXT');
+			    
+		$scraper = new Diggin_Scraper();
+		$scraper->process('//td[@class="day" and @height < 100]', 'date => "TEXT"')
+				->process('//table[@width="306"]', array('ranking[]' => $ranking))
+				->scrape($url);
+} catch (Diggin_Scraper_Exception $e) {
+	    die($e->getMessage());
+}
+
+Zend_Debug::dump($scraper->results);
+
