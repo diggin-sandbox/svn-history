3129ec8af41623d68c7a23f8ef0e804adcd40263
sasezaki <sasezaki@b45cdfdc-3f41-0410-be57-3dd150e55053>
2008-09-27 07:51:51 +0000

    indent

--- trunk/demos/Diggin/Scraper/uranai1.php
+++ trunk/demos/Diggin/Scraper/uranai1.php
@@ -5,18 +5,18 @@ Zend_Loader::registerAutoload();
 $url = 'http://www.fujitv.co.jp/meza/uranai/';
 
 try {
-	    $ranking = new Diggin_Scraper_Process();
-		$ranking->addProcess('.', 'rank => [@background, "Digits"]')
-				->addProcess('img', 'star => @alt', 'image => @src')
-			    ->addProcess('td.text', 'text => TEXT')
+        $ranking = new Diggin_Scraper_Process();
+        $ranking->addProcess('.', 'rank => [@background, "Digits"]')
+                ->addProcess('img', 'star => @alt', 'image => @src')
+                ->addProcess('td.text', 'text => TEXT')
                 ->addProcess('.lucky', 'lucky => TEXT');
-			    
-		$scraper = new Diggin_Scraper();
-		$scraper->process('//td[@class="day" and @height < 100]', 'date => "TEXT"')
-				->process('//table[@width="306"]', array('ranking[]' => $ranking))
-				->scrape($url);
+
+        $scraper = new Diggin_Scraper();
+        $scraper->process('//td[@class="day" and @height < 100]', 'date => "TEXT"')
+                ->process('//table[@width="306"]', array('ranking[]' => $ranking))
+                ->scrape($url);
 } catch (Diggin_Scraper_Exception $e) {
-	    die($e->getMessage());
+         die($e->getMessage());
 }
 
 Zend_Debug::dump($scraper->results);
