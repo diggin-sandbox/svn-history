5f48f3f62cecf0107487f8b02f92bcc4afc78618
sasezaki <sasezaki@b45cdfdc-3f41-0410-be57-3dd150e55053>
2008-10-03 13:33:49 +0000

    リニューアルに伴いhatenakeywordのデモ修正

--- trunk/demos/Diggin/Scraper/hatenakeyword.php
+++ trunk/demos/Diggin/Scraper/hatenakeyword.php
@@ -1,13 +1,17 @@
 <?php
 require_once 'Diggin/Scraper.php';
-$scraper = new Diggin_Scraper();
-$scraper->changeStrategy("Diggin_Scraper_Strategy_Selector")
-        ->process('span.title > a:first-child', "title => 'TEXT'", "url => '@href'")
-        ->process('span.furigana', "furigana => 'TEXT'")
-        ->process('ul.list-circle > li:first-child > a', "category => 'TEXT'")
-        ->scrape("http://d.hatena.ne.jp/keyword/%BA%B0%CC%EE%A4%A2%A4%B5%C8%FE");
-print_r($scraper->results);
 
+try {
+    $scraper = new Diggin_Scraper();
+    $scraper->process('a.title', "title => 'TEXT'", "url => '@href'")
+            ->process('span.furigana', "furigana => 'TEXT'")
+            ->process('span.category > a', "category => 'TEXT'")
+            ->scrape('http://d.hatena.ne.jp/keyword/%BA%B0%CC%EE%A4%A2%A4%B5%C8%FE');
+    print_r($scraper->results);
+    
+} catch (Exception $e) {
+    die($e);
+}
 /* print_r results:
 Array
 (
@@ -15,11 +19,9 @@ Array
     [url] => http://d.hatena.ne.jp/keyword/%ba%b0%cc%ee%a4%a2%a4%b5%c8%fe
     [furigana] => こんのあさみ
     [category] => アイドル
-)
+    )
 */
-
-//Web::Scraper's eg ....
-
+//Web::Scraper's eg ... (but this is not work, because recent renewal)
 //my $keyword = scraper {
 //    process 'span.title > a:first-child', title => 'TEXT', url => '@href';
 //    process 'span.furigana', furigana => 'TEXT';
