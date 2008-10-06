78bc32f03997b78d598916c163717235076997ee
sasezaki <sasezaki@b45cdfdc-3f41-0410-be57-3dd150e55053>
2008-10-06 14:09:53 +0000

    demo追加

---
+++ trunk/demos/Diggin/Scraper/20081006_hatenaMidashi.php
@@ -0,0 +1,59 @@
+<?php
+//@see http://d.hatena.ne.jp/kane46taka/20081002/1222949472
+require_once 'Diggin/Scraper.php';
+
+$section = new Diggin_Scraper_Process();
+$section->addProcess('h3.title', 'title => TEXT')
+        ->addProcess('h3.title a', 'link => @href');
+
+$scraper = new Diggin_Scraper();
+$scraper->process('div.section', array('section[]' => $section))
+        ->scrape('http://d.hatena.ne.jp/amachang');
+        
+print_r($scraper->results);
+
+/*****
+Web_Scraper.php's result
+
+array(8) {
+  [0]=>
+  array(2) {
+    ["text"]=>
+    string(27) "主に金沢関係の人へ"
+    ["attributes"]=>
+    array(1) {
+      ["href"]=>
+      string(29) "/amachang/20081006/1223277205"
+    }
+  }
+  [1]=>
+  array(2) {
+    ["text"]=>
+    string(75) "テンプレートのメンバ関数がインスタンス化される箇所"
+    ["attributes"]=>
+    array(1) {
+      ["href"]=>
+      string(29) "/amachang/20081005/1223225282"
+    }
+  }
+ */
+
+/**
+Diggin Scraper's result
+Array
+(
+    [section] => Array
+        (
+            [0] => Array
+                (
+                    [title] => 主に金沢関係の人へ
+                    [link] => http://d.hatena.ne.jp/amachang/20081006/1223277205
+                )
+
+            [1] => Array
+                (
+                    [title] => テンプレートのメンバ関数がインスタンス化される箇所
+                    [link] => http://d.hatena.ne.jp/amachang/20081005/1223225282
+                )
+'
+ */
