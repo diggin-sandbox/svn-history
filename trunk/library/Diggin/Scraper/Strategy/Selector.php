1a16cd85462e275eb789e17837be88776ac38034
sasezaki <sasezaki@b45cdfdc-3f41-0410-be57-3dd150e55053>
2008-11-22 20:16:49 +0000

    sfDomCssSelectorの同梱とりやめ

--- trunk/library/Diggin/Scraper/Strategy/Selector.php
+++ trunk/library/Diggin/Scraper/Strategy/Selector.php
@@ -15,7 +15,7 @@
  */
 
 require_once 'Diggin/Scraper/Strategy/Abstract.php';
-require_once dirname(__FILE__).'/Selector/sfDomCssSelector.class.php';
+require_once 'symfony/util/sfDomCssSelector.class.php';
 class Diggin_Scraper_Strategy_Selector extends Diggin_Scraper_Strategy_Abstract 
 {    
     protected static $_adapter = null;
