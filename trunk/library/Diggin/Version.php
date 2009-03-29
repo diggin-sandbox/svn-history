740eddabe14a773ed2e493ebd64d347360e0d13a
sasezaki <sasezaki@b45cdfdc-3f41-0410-be57-3dd150e55053>
2009-03-29 10:11:15 +0000

    忘れないうちに6.3としてバージョンコミット

--- trunk/library/Diggin/Version.php
+++ trunk/library/Diggin/Version.php
@@ -25,7 +25,7 @@ final class Diggin_Version
      * library version
      *
      */
-    const VERSION = '0.6.2';
+    const VERSION = '0.6.3';
 
     /**
      * Compare the specified library version string $version
