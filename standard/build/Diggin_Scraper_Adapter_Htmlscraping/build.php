3e799378a7d973841c0a3c35226e34aae2b7bd15
sasezaki <sasezaki@b45cdfdc-3f41-0410-be57-3dd150e55053>
2009-05-16 05:47:54 +0000

    move

--- standard/build/Diggin_Scraper_Adapter_Htmlscraping/build.php
+++
@@ -1,9 +0,0 @@
-<?php
-$adapterpath = dirname(dirname(dirname(realpath(__FILE__)))).'/library/Diggin/Scraper/Adapter/';
-// copy file this directory
-copy($adapterpath.'Htmlscraping.php', 'Htmlscraping.php');
-// make package2.xml
-include 'makepackage_Diggin_Scraper_Adapter_Htmlscraping.php';
-exec('sudo pear package package2.xml');
-// delete file
-unlink('Htmlscraping.php');
