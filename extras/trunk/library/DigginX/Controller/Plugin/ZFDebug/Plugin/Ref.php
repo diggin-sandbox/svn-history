1deb622df406e7da0869e0145880d70fd7ce60fc
sasezaki <sasezaki@b45cdfdc-3f41-0410-be57-3dd150e55053>
2009-05-24 09:57:11 +0000

    initial commit DigginX

---
+++ extras/trunk/library/DigginX/Controller/Plugin/ZFDebug/Plugin/Ref.php
@@ -0,0 +1,124 @@
+<?php
+
+require_once 'ZFDebug/Controller/Plugin/Debug/Plugin/File.php';
+
+class DigginX_Controller_Plugin_ZFDebug_Plugin_Ref
+        extends ZFDebug_Controller_Plugin_Debug_Plugin_File
+{
+    protected $_sourcePath = array();
+
+    protected $_manualPath = array();
+
+    public function __construct(array $options = array())
+    {
+        parent::__construct($options);
+        
+        //default source path
+        $this->setSourcePath('Zend', 'http://framework.zend.com/code/browse/Standard_Library/standard/trunk/library/', '?r=trunk');
+        $this->setSourcePath('ZendX', 'http://framework.zend.com/code/browse/Extras_Library/trunk/library/', '?r=trunk');
+        $this->setSourcePath('ZFDebug', 'http://code.google.com/p/zfdebug/source/browse/trunk/library/');
+        
+        //$this->_sourcePath = array_merge($sourcePath, $options['source_path']);
+    }
+
+    /**
+     * set reference path and suffix
+     *
+     * there is a lot of path....
+     * http://framework.zend.com/svn/framework/standard/branches/release-1.8/library/
+     * http://framework.zend.com/svn/framework/standard/tags/release-1.8.1/library/
+     * http://framework.zend.com/code/browse/Standard_Library/standard/trunk/library/Zend/Filter/Input.php?r=trunk
+     * 
+     * //
+     * require_once 'Zend/Version.php';
+     * $version = Zend_Version::VERSION; 
+     * @todo not-stable (dev version 1.8.0dev-etc)'s repo not exists;
+     * $this->setSourcePath('Zend', "http://framework.zend.com/svn/framework/standard/tags/release-$version/library/");
+     *
+     * $this->setSourcePath('Zend', 'http://framework.zend.com/code/browse/Standard_Library/standard/trunk/library/', '?r=trunk');
+     * $this->setSourcePath('ZendX', 'http://framework.zend.com/code/browse/Extras_Library/trunk/library/', '?r=trunk');
+     * //
+     *
+     * @param string $libraryName
+     * @param string $sourcePath
+     * @param string $suffix
+     */
+    public function setSourcePath($libraryName, $sourcePath, $suffix = null)
+    {
+        $this->_sourcePath = array_merge($this->_sourcePath, 
+                                   array($libraryName => array('src' => $sourcePath, 'suffix' => $suffix)));
+    }
+
+    /**
+     *
+     */
+    public function setManualPath($libraryName, $manualPath)
+    {
+        $this->_manualPath = array_merge($this->_manualPath, array($libraryName => $manualPath));
+    }
+
+    protected function getManualPath($file)
+    {
+        if ($this->_manualPath = array()) {
+            return '';
+        }
+
+    }
+
+    public function getPanel()
+    {
+
+        $included = $this->_getIncludedFiles();
+        $html = '<h4>File Information</h4>';
+        $html .= count($included).' Files Included<br />';
+        $size = 0;
+        foreach ($included as $file) {
+            $size += filesize($file);
+        }
+        $html .= 'Total Size: '. round($size/1024, 1).'K<br />';
+        
+        $html .= 'Basepath: ' . $this->_basePath . '<br />';
+
+        $libraryFiles = array();
+        foreach ($this->_library as $key => $value) {
+            if ('' != $value) {
+                $libraryFiles[$key] = '<h4>' . $value . ' Library Files</h4>';
+            }
+        }
+
+        $html .= '<h4>Application Files</h4>';
+        foreach ($included as $file) {
+            $file = str_replace($this->_basePath, '', $file);
+            $inUserLib = false;
+                foreach ($this->_library as $key => $library)
+                {
+                        if('' != $library && false !== strstr($file, $library)) {
+                                if (isset($this->_sourcePath[$library])) {
+                                    
+                                    // clear include_path 
+                                    $path = str_replace('.', '', explode(PATH_SEPARATOR, get_include_path()));
+                                    $file = substr(str_replace($path, '' , $file), 1);
+                                    $souceBase = $this->_sourcePath[$library]['src'];
+                                    $suffix = $this->_sourcePath[$library]['suffix'];
+
+                                    $libraryFiles[$key] .= $file .' <a href="'.$souceBase.$file.$suffix.'">src</a>'.'<br />';
+                                } else {
+                                    $libraryFiles[$key] .= $file .'<br />';
+                                }
+                                $inUserLib = TRUE;
+                        }
+                }
+                if (!$inUserLib) {
+                        $html .= $file . '<br />';
+                }
+        }
+
+        $html .= implode('', $libraryFiles);
+
+
+
+        return $html;
+    }
+
+
+}
