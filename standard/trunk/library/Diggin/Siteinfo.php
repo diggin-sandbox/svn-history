a14b3bdb4cac0bcb0f2587292288edb4fb600967
sasezaki <sasezaki@b45cdfdc-3f41-0410-be57-3dd150e55053>
2009-07-18 13:55:18 +0000

    fixed typo

--- standard/trunk/library/Diggin/Siteinfo.php
+++ standard/trunk/library/Diggin/Siteinfo.php
@@ -1,15 +1,28 @@
 <?php
+/**
+ * Diggin - Simplicity PHP Library
+ * 
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license.
+ * http://diggin.musicrider.com/LICENSE
+ * 
+ * @category   Diggin
+ * @package    Diggin_Siteinfo
+ * @copyright  2006-2009 sasezaki (http://diggin.musicrider.com)
+ * @license    http://diggin.musicrider.com/LICENSE     New BSD License
+ */
 
 class Diggin_Siteinfo extends ArrayIterator
 {
     public function current()
     {
-        $curerent = parent::current();
-        if (is_array($curerent) && array_key_exists('data', $curerent)){
-            return $curerent['data'];
+        $current = parent::current();
+        if (is_array($current) && array_key_exists('data', $current)){
+            return $current['data'];
         }
 
-        return $curerent;
+        return $current;
     }
 }
 
