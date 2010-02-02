885de127cdfaea73ef5ba2458eb2371d42abbb5c
sasezaki <sasezaki@b45cdfdc-3f41-0410-be57-3dd150e55053>
2010-02-02 16:49:01 +0000

    Uri_Http

--- standard/trunk/library/Diggin/Uri/Http.php
+++ standard/trunk/library/Diggin/Uri/Http.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Diggin - Simplicity PHP Library
  * 
@@ -10,89 +11,56 @@
  * 
  * @category   Diggin
  * @package    Diggin_Uri
- * @copyright  2006-2008 sasezaki (http://diggin.musicrider.com)
+ * @copyright  2006-2009 sasezaki (http://diggin.musicrider.com)
  * @license    http://diggin.musicrider.com/LICENSE     New BSD License
  */
 
-
 class Diggin_Uri_Http
 {
-    
     /**
-     * Base Url 
+     * Base Uri 
      *
      * @var mixed
      */
-    protected static $baseUrl;
+    protected $_baseUri;
     
-    public static function setBaseUrl($url)
-    {
-        if ($url === false) {
-            self::$baseUrl = null;
-        }
-        
-        self::$baseUrl = $url;
+    public function setBaseUri($uri)
+    {        
+        $this->_baseUri = $uri;
     }
 
-    
     /**
      * Getting "Real" URL not URI
      * Replace HTML'S relative path
      *  
      * using pecl_http 
      * 
-     * @param string $url
-     * @param string $baseUrl 
-     *  (Not directoy, this param must set URL work as file(HTMLorXML))
-     * @return string
+     * @param string $target
+     * @return string     
      */
-    public static function getAbsoluteUrl($url, $baseUrl)
-    {
-        //using pecl_http
-        if (extension_loaded('http')) {
-            //static $baseUrl;
-            if (array_key_exists('host', parse_url($url))) {
-                return $url;
-            } else {
-                if (strpos(pathinfo(parse_url($baseUrl, PHP_URL_PATH), PATHINFO_DIRNAME), '/') === false) {
-                    return http_build_url($baseUrl, array("path" => $url),
-                    HTTP_URL_STRIP_QUERY | HTTP_URL_STRIP_FRAGMENT);
-                } else {            
-                    return http_build_url($baseUrl, array("path" => $url), 
-                    HTTP_URL_JOIN_PATH | HTTP_URL_STRIP_QUERY | HTTP_URL_STRIP_FRAGMENT);
-                }
-            }
-        //Net_URL2 ver 0.2.0
-        } else {
-            if (!class_exists('Net_URL2')) require_once 'Net/URL2.php';
-            $neturl2 = new Net_URL2($baseUrl);
-            // @todo ブラウザ側でrawurlencodeしてそうという補正
-            return $neturl2->resolve(str_replace(chr(32), '%20', $url))->getUrl();
-        } 
-    }
     
-    public static function getAbsoluteUrl2($url)
+    public function getAbsoluteUrl($target)
     {
         //using pecl_http
         if (extension_loaded('http')) {
-            if (array_key_exists('host', parse_url($url))) {
-                return $url;
+            if (array_key_exists('host', parse_url($target))) {
+                return $target;
             } else {
-                if (strpos(pathinfo(parse_url(self::$baseUrl, PHP_URL_PATH), PATHINFO_DIRNAME), '/') === false) {
-                    return http_build_url(self::$baseUrl, array("path" => $url),
+                if (strpos(pathinfo(parse_url($this->_baseUri, PHP_URL_PATH), PATHINFO_DIRNAME), '/') === false) {
+                    return http_build_url($this->_baseUri, array("path" => $target),
                     HTTP_URL_STRIP_QUERY | HTTP_URL_STRIP_FRAGMENT);
                 } else {            
-                    return http_build_url(self::$baseUrl, array("path" => $url), 
+                    return http_build_url($this->_baseUri, array("path" => $target), 
                     HTTP_URL_JOIN_PATH | HTTP_URL_STRIP_QUERY | HTTP_URL_STRIP_FRAGMENT);
                 }
             }
         //Net_URL2 ver 0.2.0
         } else {
-            if (is_string(self::$baseUrl)) {
+            if (is_string($this->_baseUri)) {
                 require_once 'Net/URL2.php';
-                self::$baseUrl = new Net_URL2(self::$baseUrl);
+                $this->_baseUri = new Net_URL2($this->_baseUri);
             }
-            return self::$baseUrl->resolve($url)->getUrl();
+            return $this->_baseUri->resolve($target)->getUrl();
         } 
     }
 }
