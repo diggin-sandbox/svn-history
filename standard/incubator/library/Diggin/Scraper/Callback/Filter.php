784ec4b5836acbb9ef44b09b41709605ab3af235
sasezaki <sasezaki@b45cdfdc-3f41-0410-be57-3dd150e55053>
2010-02-02 15:30:11 +0000

    refactor Strategy_Callback 2 Callback_Evaluator

---
+++ standard/incubator/library/Diggin/Scraper/Callback/Filter.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Diggin - Simplicity PHP Library
+ * 
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license.
+ * It is also available through the world-wide-web at this URL:
+ * http://diggin.musicrider.com/LICENSE
+ * 
+ * @category   Diggin
+ * @package    Diggin_Scraper
+ * @copyright  2006-2009 sasezaki (http://diggin.musicrider.com)
+ * @license    http://diggin.musicrider.com/LICENSE     New BSD License
+ */
+
+require_once 'Diggin/Scraper/Filter.php';
+
+class Diggin_Scraper_Callback_Filter extends IteratorIterator
+{
+    public function __construct(Diggin_Scraper_Callback_Evaluator $callback)
+    {
+        if ($filters = $callback->getProcess()->getFilters()) {
+            $callback = $this->getFilters($callback, $filters);
+        }
+
+        return parent::__construct($callback);
+    }
+    
+    protected function getFilters($iterator, $filters)
+    {
+        foreach ($filters as $filter) {
+            $iterator = Diggin_Scraper_Filter::factory($iterator, $filter);
+        }
+
+        return $iterator;
+    }
+
+}
