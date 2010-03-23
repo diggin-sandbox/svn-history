e0221df8d19dd9c36afd7b8fa3b83eac01067b82
sasezaki <sasezaki@b45cdfdc-3f41-0410-be57-3dd150e55053>
2010-03-23 15:13:00 +0000

    version as dev

--- standard/trunk/library/Diggin/Version.php
+++ standard/trunk/library/Diggin/Version.php
@@ -25,7 +25,7 @@ final class Diggin_Version
      * library version
      *
      */
-    const VERSION = '0.7.0';
+    const VERSION = '0.7.0dev';
 
     /**
      * Compare the specified library version string $version
